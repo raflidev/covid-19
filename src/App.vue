0bba86b3da0f91657069aa109d6f308fb04346f2
Rafli Ramadhan <rafliramadhan@gmail.com>
2020-04-11 23:56:26 +0700

    font fix

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&display=swap');
 body{
-  font-family: 'Montserrat', sans-serif;
+  font-family: 'Montserrat', sans-serif!important;
 }
 </style>
 <script>
